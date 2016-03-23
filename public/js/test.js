b38d65a948f269bbb20d26ce73c76376ccb7f12a
bharathrangaraj <bharathrwth@gmail.com>
2016-03-23 12:11:40 +0100

    testing embeds initial

--- public/js/test.js
+++ public/js/test.js
@@ -1,9 +1,9 @@
 /**
  * Created by Bharath on 22/03/16.
  */
-var scrape=require("/Users/Bharath/WebstormProjects/LearningHub/public/js/scrape.js");
-var link="www.v3club.net";
+var htm="<iframe height=\"120\" frameborder=\"0\" width=\"380\" scrolling=\"NO\" src=\"http:\/\/chirb.it\/wp\/OBnAr1\">This browser does not show iframe content. Listen to this chirbit here <a href=\"http:\/\/chirb.it\/OBnAr1\">http:\/\/chirb.it\/OBnAr1<\/a><\/iframe>";
+//console.log("prev"+htm);
+htm.replace('/\"/g','"');
+process.stdout.write(htm);
 
-scrape.getInfo(link,function(res){
 
-});
